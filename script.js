08e42227f0257d02f96fe31c3477b7594c1f56e7
petetes93 <alejandrolobatohaurie@gmail.com>
2023-10-10 10:08:26 +0200

    kata_pelea_azt_maya_update

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ class Maya extends Warrior {
     }
 
     colacao() {
-        console.log(`${this.nombre} bebe ColaCao y suma 10 de vida.`);
-        this.life += 10;
+        console.log(`${this.nombre} bebe ColaCao y suma 10 de poder.`);
+        this.power += 10;
     }
 }
 
@@ -50,7 +50,10 @@ const maya1 = new Maya("Maya");
 const aztec1 = new Aztec("Azteca");
 
 maya1.colacao();
+console.log(maya1);
+
 aztec1.nesquik();
+console.log(aztec1);
 
 maya1.attack(aztec1);
 aztec1.defend();
